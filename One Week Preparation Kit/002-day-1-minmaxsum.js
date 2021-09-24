f235e5ee7fa59f6a753f0983269ac555733e0987
emre <emrsmrl@gmail.com>
2021-09-24 13:10:44 +0300

    One Week Kit | Solution 2.

---
+++ One Week Preparation Kit/002-day-1-minmaxsum.js
@@ -0,0 +1,21 @@
+let arr = [1,3,9,5,4]
+function miniMaxSum() {
+    // Write your code here
+    let sorted = arr.sort(function(a,b){return a-b})
+    // console.log(sorted)
+    let maxSum = 0;
+    let minSum = 0;
+
+    for( let i = 1; i < sorted.length ; i++){
+        maxSum += Number(sorted[i]);
+    };
+    console.log(maxSum)
+
+    for(let j = 0 ; j < sorted.length-1;j++){
+        minSum += Number(sorted[j]);
+    }
+    console.log(minSum)
+
+
+}
+miniMaxSum()
